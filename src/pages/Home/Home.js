1668bc621c249462f1a3c8bfe0acfa37a29ab1f6
karthikeyan T <karthikeyant@tekion.com>
2023-07-14 10:12:33 +0530

    initial design

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import styles from './Home.module.scss'
-
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const Home = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.sectionOne}></div>
+            <div className={styles.sectionOne}>
+                <div className={styles.title}>Buy, Sell, Rent.</div>
+                <div className={styles.heading}>The best deals, for both Realtors and Customers.</div>
+                <div className={styles.subHeading}><span>Explore More </span><ArrowForwardIcon /></div>
+            </div>
             <div className={styles.sectionTwo}>test 2 data</div>
             <div className={styles.sectionThree}>test data</div>
 
