1ef75b1e1356348a4c49717ba2a5101708aaf21f
karthikeyan T <karthikeyant@tekion.com>
2023-07-16 11:14:56 +0530

    initial design

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -6,9 +6,10 @@ import house1 from './house1.jpg'
 import house2 from './house2.jpg'
 import house3 from './house3.jpg'
 import house4 from './house4.jpg'
-
-
+// import comingSoon from './comingSoon.svg'
+import underConstruction from './underConstruction.gif'
 
 export const images =  {
-    homeBg, process1, process2, process3, house1, house2, house3, house4
+    homeBg, process1, process2, process3, house1, house2, house3, house4,
+    underConstruction
 }
--- src/components/ComingSoonPage/ComingSoonPage.js
+++ src/components/ComingSoonPage/ComingSoonPage.js
@@ -1,35 +1,24 @@
 import React from 'react';
 import styles from './ComingSoonPage.module.scss';
 import { Link } from 'react-router-dom';
+import { images } from '../../assets/images';
 
 const DefaultConfig = {
   title: 'Coming Soon',
   description: 'We are working on something awesome!',
-  details: [
-    { label: 'Date', value: '2023-07-31' },
-    { label: 'Time', value: '10:00 AM' },
-  ],
   buttonText: 'Go to Home',
+  imgSrc: images.underConstruction,
 };
 
 const ComingSoonPage = ({ config = DefaultConfig }) => {
-  const { title, description, details, buttonText } = config;
+  const { title, description, buttonText, imgSrc } = config;
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>{title}</h1>
         <p className={styles.description}>{description}</p>
-        {details && (
-          <div className={styles.details}>
-            {details.map((detail, index) => (
-              <p className={styles.detailItem} key={index}>
-                <span className={styles.detailLabel}>{detail.label}: </span>
-                {detail.value}
-              </p>
-            ))}
-          </div>
-        )}
+        <img src={imgSrc} alt={title} />
         <button className={styles.button}><Link to={'/'}>{buttonText}</Link></button>
       </div>
     </div>
